ae78157ab09be4b7c659e3982ef9c7526361e732
recipepa <72211263+recipepa@users.noreply.github.com>
2020-10-01 19:57:54 +0530

    Create Guess.py
    A random number guessing game

---
+++ Guess.py
@@ -0,0 +1,11 @@
+Print("*********Guess the number game***********")
+import random
+Var_1 = random.randint(0,10)
+Var_2  = input("Enter a number")
+if(Var_1>Var_2)
+print("Var_1 is greater")
+elif(Var1<Var_2)
+print("Var_2 is greater")
+else 
+print("invalid number")
+
